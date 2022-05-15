3e1ae11faf2fe36a29acafcb10395b46d5619ada
arikz-tech <arikz15@gmail.com>
2022-05-15 20:33:44 +0300

    first commit

---
+++ js/register-logic.js
@@ -0,0 +1,10 @@
+$("#registerButton").click(() => {
+  var firstName = $("#firstName").val();
+  var lastName = $("#lastName").val();
+  var email = $("#email").val();
+  var password = $("#password").val();
+  var repeatPassword = $("#repeatPassword").val();
+  var promoCode = $("#promoCode").val();
+
+  alert(firstName + lastName);
+});
