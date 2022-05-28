9dc6657892f5568cbe8a769cd90761de0cade65c
dvirbens <dvirbens@gmail.com>
2022-05-28 20:31:50 +0300

    i love you

--- server.js
+++ server.js
@@ -4,9 +4,41 @@ var port = process.env.PORT || 8080;
 var path = require("path");
 var bodyParser = require("body-parser"); //parse request parameters
 
+
+var dbURI= 'mongodb+srv://Ada:server1234@cluster0.6afznb2.mongodb.net/?retryWrites=true&w=majority';
+var mongoose = require("mongoose");
+mongoose.connect(dbURI).then((result)=>console.log("DB connected")).catch((err)=>console.log(err));
+
 app.use(express.static(__dirname)); //specifies the root directory from which to serve static assets [images, CSS files and JavaScript files]
 app.use(bodyParser.urlencoded({ extended: true })); //parsing bodies from URL. extended: true specifies that req.body object will contain values of any type instead of just strings.
 app.use(bodyParser.json()); //for parsing json objects
+
+
+const userSchema = new mongoose.Schema ({
+  firstname:{
+    type:String,
+    required:true
+  },
+  lastname:{
+    type:String,
+    required:true
+  },
+  email:{
+    type:String,
+    required:true
+  },
+  password:{
+    type:String,
+    required:true
+  },
+  promocode:{
+    type:String,
+    required:true
+  }
+})
+
+const User= mongoose.model("Users",userSchema)
+
 
 app.get("/", function (req, res) {
   res.sendFile(path.join(__dirname + "/homePage.html"));
@@ -24,9 +56,18 @@ app.get("/dashboard", function (req, res) {
   res.sendFile(path.join(__dirname + "/dashboard.html"));
 });
 app.post("/register", (req, res) => {
-  user = req.body.user;
+  newUser = req.body.user;
   console.log("check");
-  console.log(JSON.stringify(user));
+  console.log(JSON.stringify(newUser));
+  const user= new User({
+    firstname:newUser.firstName,
+    lastname:newUser.lastName,
+    email:newUser.email,
+    password:newUser.password,
+    promocode:newUser.promoCode
+  });
+  user.save();
+  //res.send("user: " + req.body.user + " password: " + req.body.password);
 });
 
 app.get("/test_data_transfer", function (req, res) {
@@ -35,3 +76,6 @@ app.get("/test_data_transfer", function (req, res) {
 
 app.listen(port);
 console.log("Server started! At http://localhost:" + port);
+
+
+
