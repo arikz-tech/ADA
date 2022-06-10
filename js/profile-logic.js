4493f17eeb8015c1368d16e53e03f3af8d1d02e7
Aviel <aviel1440@gmail.com>
2022-06-10 16:36:47 +0300

    fetch data

--- js/profile-logic.js
+++ js/profile-logic.js
@@ -7,6 +7,11 @@ function loadData()
         dataType: 'json',
         success: function (data){
             console.log(JSON.stringify(data));
+            $.each(data, function(key, val) {
+                console.log(`${key} = ${val}`);
+                $('#'+`${key}`).html(`${val}`);
+                $('#'+`${key}`).css({'font-size': '24px', 'font-weight': 'bold', 'color': 'mediumblue'});
+            });
         }
     });
 }
